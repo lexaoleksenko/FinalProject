f96177802c91a4435cf6e52e54c09c7f0a0db114
Oleksiy Oleksenko <leshanupesu@gmail.com>
2023-04-05 13:36:50 +0300

    Keep calm and commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import Container from "@mui/material/Container";
+// import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <>
+//       <Header />
+//       <Container maxWidth="lg">
+//         <Routes>
+//           <Route path="/" element={}/>
+//         </Routes>
+//       </Container>
+//     </>
+//   );
+// }
+
+// export default App;
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://final_project:final123@finalproject.1h7yx8t.mongodb.net/test",
+  secretOrKey: "random very very secret string",
+};
