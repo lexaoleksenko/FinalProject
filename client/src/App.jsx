412918f90f92811cc434dcb01dab4ce7cb2851cd
DmytroMuzychuk <dimamu12345@gmail.com>
2023-04-13 19:40:04 +0300

    ProductCardShortInfo+ProductFilter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
-// import { Routes, Route } from "react-router-dom";
-
 import Navbar from './components/Smart/Navbar/Navbar';
 import Footer from './components/Simple/Footer/Footer';
-
 import HomePage from './pages/Home/HomePage';
 
 function App() {
---
+++ client/src/components/Simple/ProductAccordion/ProductAccordion.jsx
@@ -0,0 +1,168 @@
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Checkbox from '@mui/material/Checkbox';
+import NonLinearSlider from '../NonLinearSlider/NonLinearSlider';
+import PriceSlider from '../PriceSlider/PriceSlider';
+
+import style from './ProductAccordion.module.scss';
+
+function SimpleAccordion() {
+  return (
+    <Accordion className={style.accordion}>
+      <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+        <Typography>Goods</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <div>
+          <Accordion className={style.accordion}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>Phones</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div>
+                <Accordion className={style.accordion}>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Brand</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <div className={style.checkbox}>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        iPhone
+                      </span>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        Samsung
+                      </span>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        Huawei
+                      </span>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        Xiaomi
+                      </span>
+                    </div>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion className={style.accordion}>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Price</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <PriceSlider />
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion className={style.accordion}>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Phone memory capacity</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <NonLinearSlider />
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion className={style.accordion}>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Display</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <div className={style.checkbox}>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        LCD
+                      </span>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        OLED
+                      </span>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        AMOLED
+                      </span>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        Retina
+                      </span>
+                    </div>
+                  </AccordionDetails>
+                </Accordion>
+                <Accordion className={style.accordion}>
+                  <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Color</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                    <div className={style.checkbox}>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        Black
+                      </span>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        White
+                      </span>
+                      <span>
+                        <Checkbox
+                          inputProps={{ 'aria-label': 'Checkbox demo' }}
+                        />
+                        Other
+                      </span>
+                    </div>
+                  </AccordionDetails>
+                </Accordion>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+        <div>
+          <Accordion className={style.accordion}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+              <Typography>Accessories</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className={style.checkbox}>
+                <span>
+                  <Checkbox inputProps={{ 'aria-label': 'Checkbox demo' }} />
+                  Сovers
+                </span>
+                <span>
+                  <Checkbox inputProps={{ 'aria-label': 'Checkbox demo' }} />
+                  Headphones
+                </span>
+                <span>
+                  <Checkbox inputProps={{ 'aria-label': 'Checkbox demo' }} />
+                  Charger
+                </span>
+              </div>
+            </AccordionDetails>
+          </Accordion>
+        </div>
+      </AccordionDetails>
+    </Accordion>
+  );
+}
+
+export default SimpleAccordion;
